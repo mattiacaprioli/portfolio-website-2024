7f69ec435da40c49894558a4406a2fc6d8e29d7d
Mattia Caprioli <capriolimattia1994@gmail.com>
2025-01-17 00:17:15 +0100

    feat new project in portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ const Project = ({item}) => {
   return (
     <div key={item.id} className='flex flex-col items-center text-center'>
       <div className='mb-8'>
-        <a href={item.href}><img className='rounded-2xl' src={item.image} alt={item.name} /></a>
+        <a href={item.href}><img className='rounded-2xl max-h-[135px]' src={item.image} alt={item.name} /></a>
       </div>
       <p className='capitalize text-accent text-sm mb-3'>{item.category}</p>
       <h3 className='text-2xl font-semibold capitalize mb-3'>{item.name}</h3>
